54469920d00047c569a45545e1864c8e63f767f6
KingBubIII <richardsoncaleb60@gmail.com>
2024-12-23 22:08:34 -0700

    changed method for converting qr code img to qr code bits (not working)

--- QRCodeReader/main.cpp
+++ QRCodeReader/main.cpp
@@ -51,14 +51,14 @@ Mat get_code_square(Mat& qr_code)
     }
 
     //return std::pair<Point, Point>(top_right, bottom_left);
-    Mat crop = qr_code(Rect(bottom_left.x, top_right.y, top_right.x - bottom_left.x, bottom_left.y - top_right.y)); // Slicing to crop the imag
+    Mat crop = qr_code(Rect(bottom_left.x, top_right.y, top_right.x - bottom_left.x + 1, bottom_left.y - top_right.y + 1)); // Slicing to crop the imag
     return crop;
 }
 
 int get_pixel_width(Mat& cropped_code)
 {
     int black_pixel_value = 127;
-    float finder_pattern_width = 0;
+    int finder_pattern_width = 0;
 
     for (int col = 0; col < cropped_code.cols-1; col++)
     {
@@ -66,7 +66,7 @@ int get_pixel_width(Mat& cropped_code)
 
         if (pixel_val > black_pixel_value)
         {
-            finder_pattern_width = col-1;
+            finder_pattern_width = col;
             break;
         }
     }
@@ -74,22 +74,30 @@ int get_pixel_width(Mat& cropped_code)
     return finder_pattern_width / 7;
 }
 
-std::vector<std::vector<bool>> get_bitified_code(Mat& cropped_code, int pixel_size)
+Mat get_bitified_code(Mat& cropped_code, int pixel_size)
 {
     int black_pixel_threshold = 127;
-    std::vector<std::vector<bool>> bitified_qr_code = {};
+    Mat bitified_qr_code = Mat(cropped_code.rows / pixel_size, cropped_code.cols / pixel_size, CV_8UC1);
 
-    for (int row = 0; row < cropped_code.rows; row++)
+    std::cout << bitified_qr_code.type() << std::endl;
+    int row = 0;
+    while (row <= cropped_code.rows - (pixel_size - 1))
     {
-        bitified_qr_code.push_back({});
-        for (int col = 0; col < cropped_code.cols; col++)
+        int col = 0;
+        while (col <= cropped_code.cols - (pixel_size-1))
         {
-            Mat curr_pixel = cropped_code(Rect(col * pixel_size, row * pixel_size, pixel_size, pixel_size));
+            Mat curr_pixel = cropped_code(Rect(col, row, pixel_size-1, pixel_size-1));
             Scalar asdf = mean(curr_pixel);
             int avg_pixel_val = asdf(0);
 
-            bitified_qr_code[row].push_back(avg_pixel_val > black_pixel_threshold);
+            int bit_row = row / pixel_size;
+            int bit_col = col / pixel_size;
+            bitified_qr_code.at<uchar>(bit_row, bit_col) = (avg_pixel_val > black_pixel_threshold) ? 255 : 0;
+
+            col += pixel_size;
         }
+
+        row += pixel_size;
     }
 
     return bitified_qr_code;
@@ -98,33 +106,29 @@ std::vector<std::vector<bool>> get_bitified_code(Mat& cropped_code, int pixel_si
 Mat reduce_code_to_single_pixels(Mat& qr_code_img)
 {
     Mat cropped_code = get_code_square(qr_code_img);
-    //imwrite("crop.png", cropped_code);
+    imwrite("crop.png", cropped_code);
 
-    float pixel_width = get_pixel_width(cropped_code);
+    int pixel_width = get_pixel_width(cropped_code);
 
-    std::vector<std::vector<bool>> bitified_qr_code = get_bitified_code(cropped_code, pixel_width);
+    Mat bitified_qr_code = get_bitified_code(cropped_code, pixel_width);
 
-    float new_size = cropped_code.rows / pixel_width;
-
-    Mat resized_crop;
-    //resize(cropped_code, resized_crop, Size(new_size, new_size), INTER_LINEAR);
-
-    return resized_crop;
+    return bitified_qr_code;
 }
 
 int main()
 {
-    std::string path1 = "C:/Users/richa/Downloads/website_qr.png";
+    std::string path1 = "C:/Users/richa/OneDrive/Documents/programming/QRcodeReader/frame.png";
     std::string path2 = "C:/Users/richa/Pictures/Screenshots/qrcode_test1.png";
 
     // Reading image
-    Mat img = imread(path2, IMREAD_GRAYSCALE);
+    Mat org_qr_code = imread(path1, IMREAD_GRAYSCALE);
 
     // Display original image
-    imshow("original Image", img);
+    imshow("original Image", org_qr_code);
     waitKey(0);
 
-    Mat new_img = reduce_code_to_single_pixels(img);
+    Mat new_img = reduce_code_to_single_pixels(org_qr_code);
+    
     imshow("Cropped Image", new_img);
     int k = waitKey(0); // Wait for a keystroke in the window
 
